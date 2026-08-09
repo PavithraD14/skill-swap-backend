a73058c1aba166e51c1d9a26cc54c90ae9e328e5
pavithra <pavija.1417@gmail.com>
2026-08-09 12:10:28 +0530

    Fix missing uploads folder on Render

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
+const fs = require("fs");
+const path = require("path");
 
 const dns = require("dns");
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
@@ -12,15 +14,19 @@ const skillRoutes = require("./routes/skill.routes");
 const swapRequestRoutes = require("./routes/swapRequest.routes");
 const userRoutes = require("./routes/user.routes");
 
+
+const uploadsDir = path.join(__dirname, "uploads");
+if (!fs.existsSync(uploadsDir)) {
+    fs.mkdirSync(uploadsDir, { recursive: true });
+}
+
 app.use(cors());
- 
 
 app.use(express.json());
 app.use("/auth", authenticationRoutes);
 app.use("/skills", skillRoutes);
 app.use("/swap-requests", swapRequestRoutes);
 app.use("/users", userRoutes);
-const path = require("path");
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 
